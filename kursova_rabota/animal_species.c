13a85baf9bf8012035eb2c1958a0989547bdd0c0
Martin-Naval <126425118@naval-acad.bg>
2026-01-05 21:00:46 +0000

    last changes

---
+++ kursova_rabota/animal_species.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+
+void showCategory(char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Грешка: не мога да отворя файла %s\n", filename);
+        return;
+    }
+
+    char names[50][50];     // масив за имената
+    long populations[50];   // масив за популациите
+    int count = 0;
+
+    // Четене на животните от файла
+    while (fscanf(file, "%49s %ld", names[count], &populations[count]) == 2) {
+        count++;
+    }
+
+    fclose(file);
+
+    // Показване на списък
+    printf("\nМоля избери животно:\n");
+    for (int i = 0; i < count; i++) {
+        printf("%d. %s\n", i + 1, names[i]);
+    }
+    printf("%d. обратно\n", count + 1);
+
+    int choice;
+    printf("\nВъведи избор: ");
+    scanf("%d", &choice);
+
+    if (choice >= 1 && choice <= count) {
+        printf("\nВ света има приблизително %ld броя %s.\n\n",
+               populations[choice - 1], names[choice - 1]);
+    } else {
+        printf("\nВръщане...\n\n");
+    }
+}
+int main() {
+    int choice;
+
+    while (1) {
+        printf("1. Бозайници\n");
+        printf("2. Птици\n");
+        printf("3. Риби\n");
+        printf("4. Влечуги\n");
+        printf("5. Изход\n");
+        printf("Избор: ");
+
+        scanf("%d", &choice);
+
+        if (choice == 1) showCategory("mammals.txt");
+        else if (choice == 2) showCategory("birds.txt");
+        else if (choice == 3) showCategory("fish.txt");
+        else if (choice == 4) showCategory("reptiles.txt");
+        else if (choice == 5) {
+            printf("Излизане...\n");
+            break;
+        }
+        else {
+            printf("Невалиден избор!\n\n");
+        }
+    }
+
+    return 0;
+}
+
